f4193f4399210c8b8487040ae691fdf92559346a
Nicolas Melo <nicolasmelo12@gmail.com>
2024-05-17 00:55:25 -0300

    fix schema export issue

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,9 +5,19 @@ const envSchema = z.object({
   NEXT_PRIVATE_ORIGIN: z.string().url(),
 });
 
-const envClientSchema = envSchema.parse({
-  API_HOST: process.env.API_HOST,
-  NEXT_PRIVATE_ORIGIN: process.env.__NEXT_PRIVATE_ORIGIN,
-});
+const proxy = new Proxy(
+  { data: undefined as z.infer<typeof envSchema> | undefined },
+  {
+    get: (target, key: keyof z.infer<typeof envSchema>) => {
+      if (target.data === undefined) {
+        target.data = envSchema.parse({
+          API_HOST: process.env.API_HOST,
+          NEXT_PRIVATE_ORIGIN: process.env.__NEXT_PRIVATE_ORIGIN,
+        });
+      }
+      return (target?.data)[key] || "";
+    },
+  }
+);
 
-export default envClientSchema;
+export default proxy as unknown as z.infer<typeof envSchema>;
