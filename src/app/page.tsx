9d8e1370da0b04c1f3fb8e1e3c51a6f4a1d39049
Nicolas Melo <nicolasmelo12@gmail.com>
2024-05-17 01:06:12 -0300

    fix build issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import { ContentPage } from "../page";
 
 export default async function Home() {
   const url = new URL(envClientSchema.HOST + "/api/contents");
-  const data = await fetch(url, { cache: "no-cache" }).then((res) =>
-    res.json()
-  );
+  const data = await fetch(url, { cache: "no-cache" })
+    .then((res) => res.json())
+    .catch(() => ({ success: false, data: [], reason: "unknown" }));
 
   return <ContentPage contents={data.data} />;
 }
